1edb15ab0ef51513fbba33e6cd9e189e8dbc1187
Afflerbach.Simon <Afflerbach.Simon@SK-448A5BBA149D>
2015-10-09 08:21:59 +0200

    Fakultät

---
+++ Übung/src/Fakultät.java
@@ -0,0 +1,13 @@
+
+public class Fakultät {
+	
+	static int fac(int f) {
+		  return f==0?1:f*fac(f-1);}
+	
+	public static void main(String[] args) {
+		
+		 System.out.println("fac(5) = "+fac(5));
+
+	}
+
+}
--- Übung/src/Übung.java
+++ Übung/src/Übung.java
@@ -4,6 +4,7 @@ public class Übung {
 	static int quadrat(int x) {
 		 return x*x;}
 	
+	
 	public static void main(String[] args) {
 		
 		
@@ -13,8 +14,16 @@ public class Übung {
 			 System.out.println(quadrat(-12));
 			
 			
+			 
+				  System.out.println("signum(42) = ");
+				  System.out.println((42>0)?1: ((42<0)?-1:0));
+				 
+				  }
 	
 	
 	
 	}
-}
+	
+
+	
+	
