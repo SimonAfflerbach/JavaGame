42b809792dffcb6733a8930507cbfb8d9a21f610
Afflerbach.Simon <Afflerbach.Simon@SK-448A5BBA149D>
2015-10-20 15:04:31 +0200

    kkkkk

---
+++ AnimationGame/src/FirstPanel.java
@@ -0,0 +1,20 @@
+
+import javax.swing.*;
+
+public class FirstPanel extends JPanel {
+	private static final long serialVersionUID = 1L;
+
+	@Override public void paintComponent(java.awt.Graphics g) {
+		g.fillRect(30,50,45,80);
+	}
+	
+	public static void main(String [] args) {
+
+		 JFrame f=new JFrame();
+
+		 f.add(new FirstPanel());
+
+		 f. setVisible (true) ;
+	}
+		
+}
--- AnimationGame/src/JFrameTest.java
+++ AnimationGame/src/JFrameTest.java
@@ -1,5 +1,5 @@
 import javax.swing.JFrame;
-public class JFrameTest{
+public class JFrameTest {
 	public static void main(String[]args){
 		JFrame f = new JFrame("Hier könnte Ihre Werbung stehen!");
 		f.setVisible(true);
